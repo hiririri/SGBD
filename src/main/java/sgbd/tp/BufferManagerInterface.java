2e41665ca4c27f89c4ffa12a94703a83d534ffb8
Qinming JIANG <qinming.jiang@dauphine.eu>
2024-01-25 23:37:04 +0100

    complete BufferManagerInterface.Clock implement.

--- src/main/java/sgbd/tp/BufferManagerInterface.java
+++ src/main/java/sgbd/tp/BufferManagerInterface.java
@@ -1,6 +1,9 @@
 package sgbd.tp;
 
-import java.util.*;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
 
 public sealed interface BufferManagerInterface {
     void accessPage(String pageId);
@@ -74,15 +77,85 @@ public sealed interface BufferManagerInterface {
 
         @Override
         public void accessPage(String pageId) {
-            // TODO: Implement
+            Node node = find(pageId);
+            if (node != null) {
+                // Page is in memory, mark it as accessed
+                node.accessed = true;
+            } else {
+                // Page is not in memory, need to replace a page
+                misses++;
+                if (size < capacity) {
+                    // Still have room in memory, just add the page
+                    insert(pageId);
+                } else {
+                    // No room in memory, need to replace a page
+                    while (hand.accessed) {
+                        // Clear the accessed bit and move the hand
+                        hand.accessed = false;
+                        hand = hand.next;
+                    }
+                    // Replace the page the hand is pointing to
+                    hand.pageId = pageId;
+                    hand.accessed = true;
+                }
+            }
+            printBuffer(pageId);
         }
 
         private void insert(String pageId) {
-
+            Node node = new Node(pageId);
+            node.accessed = true;
+            if (head == null) {
+                node.next = node; // Point to itself
+                head = node;
+                hand = node;
+            } else {
+                // Insert the new node to the end of the circular list
+                Node tail = head;
+                while (tail.next != head) {
+                    tail = tail.next;
+                }
+                tail.next = node;
+                node.next = head; // Form a circular list
+            }
+            size++;
         }
 
         private Node find(String pageId) {
+            if (head == null) {
+                return null;
+            }
+            Node node = head;
+            do {
+                if (node.pageId.equals(pageId)) {
+                    return node;
+                }
+                node = node.next;
+            } while (node != head);
             return null;
+        }
+
+        private void printBuffer(String pageId) {
+            if (head == null) {
+                System.out.println("Buffer is empty");
+                return;
+            }
+            StringBuilder bufferStr = new StringBuilder();
+            Node node = head;
+            do {
+                bufferStr.append("[");
+                bufferStr.append(node.pageId);
+                bufferStr.append("]");
+                bufferStr.append("-");
+                bufferStr.append("(");
+                bufferStr.append(node.accessed ? 1 : 0);
+                bufferStr.append(")");
+                if (node.next != head) {
+                    bufferStr.append(", ");
+                }
+                node = node.next;
+            } while (node != head);
+            System.out.println("Buffer " + pageId + " -> : " + bufferStr);
         }
 
         public int misses() {
