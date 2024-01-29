94829c08b8966f4efb16cd2ae8c5293a970991d0
Qinming JIANG <qinming.jiang@datanumia.com>
2024-01-29 17:14:43 +0100

    commit.

--- src/main/java/sgbd/tp/BTree.java
+++ src/main/java/sgbd/tp/BTree.java
@@ -1,47 +1,114 @@
 package sgbd.tp;
 
-import java.util.*;
+import java.util.Arrays;
 
 public class BTree {
-    private static int order;
-    private List<Leaf> leaves;
+    private Node root;
+    private int t; // Minimum degree
 
-    public BTree(int order) {
-        BTree.order = order;
-        leaves = new ArrayList<>();
+    public BTree(int t) {
+        this.root = null;
+        this.t = t;
     }
 
-    public void insert(String pageId) {
-        if (leaves.stream().anyMatch(leaf -> !leaf.contains(pageId))) {
-            return;
+    // The main function that inserts a new key in this B-Tree
+    public void insert(int k) {
+        // If tree is empty
+        if (root == null) {
+            root = new Node(t, true);
+            root.keys[0] = k;
+            root.n = 1;
+        } else {
+            // If root is full, then tree grows in height
+            if (root.n == 2 * t - 1) {
+                Node s = new Node(t, false);
+                s.children[0] = root;
+                splitChild(s, 0, root);
+                int i = 0;
+                if (s.keys[0] < k) {
+                    i++;
+                }
+                insertNonFull(s.children[i], k);
+                root = s;
+            } else {
+                insertNonFull(root, k);
+            }
         }
     }
 
-    @Override
-    public String toString() {
-        return leaves.toString();
+    // A utility function to split the child y of this node
+    private void splitChild(Node x, int i, Node y) {
+        Node z = new Node(y.t, y.leaf);
+        x.children[i + 1] = z;
+        x.keys[i] = y.keys[t - 1];
+        z.n = t - 1;
+        if (!y.leaf) {
+            System.arraycopy(y.children, t, z.children, 0, t);
+        }
+        y.n = t - 1;
+        System.arraycopy(x.children, i + 1, x.children, i + 2, x.n - i);
+        System.arraycopy(x.keys, i, x.keys, i + 1, x.n - i);
+        x.children[i] = y;
+        x.n++;
     }
 
-    private record Leaf(LinkedHashSet<String> values) {
-        public void add(String pageId) {
-            values.add(pageId);
+    // A utility function to insert a new key in this node
+    private void insertNonFull(Node x, int k) {
+        int i = x.n - 1;
+        if (x.leaf) {
+            x.keys = Arrays.copyOf(x.keys, x.keys.length + 1);
+            while (i >= 0 && k < x.keys[i]) {
+                x.keys[i + 1] = x.keys[i];
+                i--;
+            }
+            x.keys[i + 1] = k;
+            x.n = x.n + 1;
+        } else {
+            while (i >= 0 && k < x.keys[i]) {
+                i--;
+            }
+            i++;
+            if (x.children[i].n == 2 * t - 1) {
+                splitChild(x, i, x.children[i]);
+                if (k > x.keys[i]) {
+                    i++;
+                }
+            }
+            insertNonFull(x.children[i], k);
         }
+    }
 
-        public boolean contains(String pageId) {
-            return values.contains(pageId);
+    // Function to traverse all nodes in a subtree rooted with this node
+    public void traverse() {
+        traverse(root);
+    }
+
+    private void traverse(Node x) {
+        int i;
+        for (i = 0; i < x.n; i++) {
+            if (!x.leaf) {
+                traverse(x.children[i]);
+            }
+            System.out.print(x.keys[i] + " ");
         }
-
-        public boolean isFull() {
-            return values.size() == order;
+        if (!x.leaf) {
+            traverse(x.children[i]);
         }
+    }
 
-        public boolean isBetween(String pageId) {
-            return values.getFirst().compareTo(pageId) < 0 && values.getLast().compareTo(pageId) > 0;
-        }
+    private class Node {
+        int[] keys;
+        int t; // Minimum degree (defines the range for number of keys)
+        Node[] children;
+        int n; // Current number of keys
+        boolean leaf; // Is true when node is leaf. Otherwise false
 
-        @Override
-        public String toString() {
-            return values.toString();
+        public Node(int t, boolean leaf) {
+            this.t = t;
+            this.leaf = leaf;
+            this.keys = new int[2 * t - 1];
+            this.children = new Node[2 * t];
+            this.n = 0;
         }
     }
 }
--- src/test/java/sgbd/tp/BTreeTest.java
+++ src/test/java/sgbd/tp/BTreeTest.java
@@ -7,20 +7,20 @@ import java.util.List;
 import java.util.stream.Stream;
 
 class BTreeTest {
-    List<String> pages;
+    List<Integer> pages;
 
     @BeforeEach
     void setUp() {
-        pages = Stream
-                .of(10, 15, 30, 27)
-                .map(String::valueOf)
-                .toList();
+        pages = Stream.of(10, 15, 30, 27, 35, 40, 45, 37, 20, 50, 55, 46, 71, 66, 74, 85, 90, 79, 78, 95, 25, 81, 68, 60, 65).toList();
     }
 
     @Test
     void test_btree() {
-        BTree btree = new BTree(5);
-        pages.forEach(btree::insert);
-        System.out.println(btree);
+        BTree btree = new BTree(3);
+
+        for (Integer page : pages) {
+            btree.insert(page);
+        }
+        btree.traverse();
     }
 }
